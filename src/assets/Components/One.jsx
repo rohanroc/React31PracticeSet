500b51dab446df6b4c79a5ac5de16836437c74d2
Arijit <saha65092@gmail.com>
2024-08-14 19:36:59 +0530

    project one added

--- src/assets/Components/One.jsx
+++ src/assets/Components/One.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function One() {
+    const [count, setCount] = useState(0);
+    function incriment() {
+        setCount(prevCount => prevCount + 1);
+    }
+    function decrement() {
+        setCount(prevCount => prevCount - 1);
+    }
     return (
         <div>
-            one
+            <button onClick={incriment}>+</button>
+            <span>{count}</span>
+            <button onClick={decrement}>-</button>
         </div>
     )
 }
